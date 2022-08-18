349c7d5ee1e2e9b187110ccff3a71d57361b9184
Kuss <kussssss@outlook.com>
2022-08-18 13:31:59 +0800

    fix: response typings

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import axios from 'axios'
 import type { AnalyticsAdminServiceClient } from '@google-analytics/admin'
-import type { BetaAnalyticsDataClient } from '@google-analytics/data'
+import { BetaAnalyticsDataClient } from '@google-analytics/data'
 
 type PickMatching<T, V> =
   { [K in keyof T as T[K] extends V ? K : never]: T[K] }
 type ExtractMethods<T> = PickMatching<T, Function>;
 
 type ParamType<T> = T extends (...args: infer U) => any ? Partial<U> : never
-type CallReturnType<T> = T extends (...args: any[]) => infer R ? R : T
 
 type ToOmit = 'initialize' | 'warn' | 'close'
 
 export type AllAdminFuncs = Omit<ExtractMethods<AnalyticsAdminServiceClient>, ToOmit>
 export type AllDataFuncs = Omit<ExtractMethods<BetaAnalyticsDataClient>, ToOmit>
+
+type ResponeType<T extends any[]> = T[0]
 
 export interface Env {
   project_id: string
@@ -22,13 +23,13 @@ export interface Env {
 
 export const GA_API = 'https://ga-functions.vercel.app/api'
 
-export const adminFetch = <T extends keyof AllAdminFuncs>(func_name: T, func_args: ParamType<AllAdminFuncs[T]>, env: Env) => {
+export const adminFetch = <T extends keyof AllAdminFuncs, K extends ParamType<AllAdminFuncs[T]>>(func_name: T, func_args: K, env: Env) => {
   const {
     project_id,
     client_email,
     private_key,
   } = env
-  return axios.post<CallReturnType<AllAdminFuncs[T]>>(`${GA_API}/admin`, {
+  return axios.post<ResponeType<K>>(`${GA_API}/admin`, {
     data: {
       project_id,
       client_email,
@@ -39,13 +40,13 @@ export const adminFetch = <T extends keyof AllAdminFuncs>(func_name: T, func_arg
   })
 }
 
-export const dataFetch = <T extends keyof AllDataFuncs>(func_name: T, func_args: ParamType<AllDataFuncs[T]>, env: Env) => {
+export const dataFetch = <T extends keyof AllDataFuncs, K extends ParamType<AllDataFuncs[T]>>(func_name: T, func_args: K, env: Env) => {
   const {
     project_id,
     client_email,
     private_key,
   } = env
-  return axios.post<CallReturnType<AllDataFuncs[T]>>(`${GA_API}/data`, {
+  return axios.post<ResponeType<K>>(`${GA_API}/data`, {
     data: {
       project_id,
       client_email,
