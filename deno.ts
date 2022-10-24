358483f49f510892aae15d798def12b203577471
Kuss <kussssss@outlook.com>
2022-10-24 22:47:24 +0800

    fix: deno deploy

--- deno.ts
+++ deno.ts
@@ -1,4 +1,16 @@
-import { serve } from "https://deno.land/std@0.149.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.160.0/http/server.ts";
+
+const parseHeaders = (originalHeaders: Request['headers']) => {
+  var headers = {};
+  originalHeaders.forEach((val, key) => {
+    headers[key] = val;
+  })
+  delete headers['host']
+  delete headers['user-agent']
+  delete headers['connection']
+  headers['content-type'] = 'application/json'
+  return headers
+}
 
 async function handleRequest(request: Request) {
   const url = new URL(request.url);
@@ -7,10 +19,14 @@ async function handleRequest(request: Request) {
   url.port = ""
   url.protocol = "https:"
 
+  const newHeaders = parseHeaders(request.headers)
+
+  const body = await request.json()
+
   const res = await fetch(url.href, {
     method: request.method,
-    headers: request.headers,
-    body: request.body
+    headers: newHeaders,
+    body: JSON.stringify(body),
   })
 
   return res;
